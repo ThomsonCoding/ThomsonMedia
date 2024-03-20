6d5a5d10a4ebd06ec006122a4c7407384e3d4ee6
Daniel Thomson <67236266+ThomsonCoding@users.noreply.github.com>
2024-03-20 17:00:40 +0000

    make carousel scroll re-usable

--- js/carouselScroll.js
+++ js/carouselScroll.js
@@ -1,38 +1,56 @@
+class Carousel {
+    constructor(parentSelector) {
+        this.parent = document.querySelector(parentSelector);
+        if (!this.parent) {
+            console.error('Parent section not found.');
+            return;
+        }
+        
+        this.carousel = this.parent.querySelector('.carousel');
+        this.prevButton = this.parent.querySelector('.carousel--button.prev');
+        this.nextButton = this.parent.querySelector('.carousel--button.next');
+        this.itemWidth = this.carousel.clientWidth;
+        this.currentScroll = 0;
+
+        if (!this.carousel || !this.prevButton || !this.nextButton) {
+            return;
+        }
+
+        this.prevButton.addEventListener('click', this.scrollLeft.bind(this));
+        this.nextButton.addEventListener('click', this.scrollRight.bind(this));
+    }
+
+    scrollLeft() {
+        this.currentScroll -= this.itemWidth;
+        if (this.currentScroll < 0) {
+            this.currentScroll = 0;
+        }
+        this.carousel.scrollTo({
+            left: this.currentScroll,
+            behavior: 'smooth'
+        });
+    }
+
+    scrollRight() {
+        const maxScroll = this.carousel.scrollWidth - this.carousel.clientWidth;
+        this.currentScroll += this.itemWidth;
+        if (this.currentScroll > maxScroll) {
+            this.currentScroll = maxScroll;
+        }
+        this.carousel.scrollTo({
+            left: this.currentScroll,
+            behavior: 'smooth'
+        });
+    }
+}
+
+// Usage
 document.addEventListener("DOMContentLoaded", function() {
-    const carousel = document.querySelector('.carousel');
-    const prevButton = document.querySelector('.carousel--button.prev');
-    const nextButton = document.querySelector('.carousel--button.next');
-    const itemWidth = carousel.clientWidth; // Use clientWidth to get visible width
-    let currentScroll = 0;
-
-    // Function to scroll carousel to the left
-    const scrollLeft = () => {
-        currentScroll -= itemWidth;
-        if (currentScroll < 0) {
-            currentScroll = 0;
+    const sections = document.querySelectorAll('section'); // Select all sections
+    sections.forEach(section => {
+        const carousel = section.querySelector('.carousel'); // Check if the section contains a carousel
+        if (carousel) {
+            const myCarousel = new Carousel(`#${section.id}`);
         }
-        carousel.scrollTo({
-            left: currentScroll,
-            behavior: 'smooth'
-        });
-    };
-
-    // Function to scroll carousel to the right
-    const scrollRight = () => {
-        const maxScroll = carousel.scrollWidth - carousel.clientWidth;
-        currentScroll += itemWidth;
-        if (currentScroll > maxScroll) {
-            currentScroll = maxScroll;
-        }
-        carousel.scrollTo({
-            left: currentScroll,
-            behavior: 'smooth'
-        });
-    };
-
-    // Event listener for previous button
-    prevButton.addEventListener('click', scrollLeft);
-
-    // Event listener for next button
-    nextButton.addEventListener('click', scrollRight);
+    });
 });
